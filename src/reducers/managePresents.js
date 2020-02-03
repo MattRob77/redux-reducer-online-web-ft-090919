d0016104e4e67c9a81d75cfabfabea0dea7b77a4
Matthew Robinson <matt_robinson1@yahoo.com>
2020-02-03 14:55:54 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,6 +2,7 @@ export function managePresents(state = { numberOfPresets:0 }, action){
   switch (action.type) {
     case "Increase": 
       return {numberOfPresets: state.numberOfPresets + 1}; 
-      default 
+      default: 
+      return state 
   }
 }
